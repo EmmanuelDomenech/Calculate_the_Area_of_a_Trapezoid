7b02c32611fec64e146c3a2c335be62389d66512
EmmanuelDomenech <82624469+EmmanuelDomenech@users.noreply.github.com>
2021-05-11 13:52:08 -0400

    Update Calculate_the_Area_of_a_Trapezoid.c

--- Calculate_the_Area_of_a_Trapezoid.c
+++ Calculate_the_Area_of_a_Trapezoid.c
@@ -1,1 +1,26 @@
+//C code
+//This program will calculate the are of a trapezoid.
+#include <stdio.h>
 
+int main()
+{
+  /* Variable definition: */
+  float baseA, baseB, height, area;
+  /* Prompt user for baseA */
+  printf("Enter the first base of the trapezoid: \n");
+  //Input the baseA
+  scanf("%f", &baseA);
+  /* Prompt user for baseB */
+  printf("Enter the second base of the trapezoid: \n");
+  //Input the baseB
+  scanf("%f", &baseB);
+  /* Prompt user for height */
+  printf("Enter the Height of the trapezoid: \n");
+  //Input the height
+  scanf("%f", &height);
+  //Calculate the area
+  area=0.5 * (baseA+baseB) * height;
+  //Print the result 
+  printf("Area is: %f \n", area);
+  return 0;
+}
